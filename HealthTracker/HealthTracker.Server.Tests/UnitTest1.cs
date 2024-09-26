66db81e3b88d467c93797a75b5f491a4ff9e1861
DamianSubzda <DamianSubzda@wp.pl>
2024-09-26 09:27:55 +0200

    Add unit test project

---
+++ HealthTracker/HealthTracker.Server.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+using HealthTracker.Server;
+using Moq;
+
+namespace HealthTracker.Server.Tests
+{
+    public class UnitTests1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
